ab3e71160fef3d93288eeee421674495c86fcd5d
Christopher-67 <christophers892@nycstudents.net>
2025-12-17 09:40:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import requests
 
+def BinaryJazz(jazz):
+    response = requests.get(f"https://binaryjazz.us/genrenator-api/{jazz.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "instruments": data["instruments"],
+        "beats": data["beats"],
+        "adjectives": data["adjectives"],
+        "prefixes": data["prefixes"],
+        "suffixes": data["suffixes"],
+        "regions": data["regions"],
+        "genres": data["genres"],
+    }
+
+pokemon = BinaryJazz("Bulbasaur")
+print(pokemon)
