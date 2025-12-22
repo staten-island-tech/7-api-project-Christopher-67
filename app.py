1680cc06cfd422bbb331b1380f955c74c7a505b9
Christopher-67 <christophers892@nycstudents.net>
2025-12-21 22:09:52 -0500

    tinkletastic

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 import requests
 import tkinter as tk
+from PIL import ImageTk, Image
+from io import BytesIO
 
 def AmiiboAPI(amiibo:str):
-    response = requests.get(f"https://www.amiiboapi.com/api/amiibo/?name={amiibo.lower()}")
+    link = f"https://www.amiiboapi.com/api/amiibo/?name={amiibo.lower()}"
+    
+    response = requests.get(link)
     if response.status_code != 200:
         print("Error fetching data!")
         return None
-    
+
     data = response.json()
 
     return data["amiibo"]
 
+def get_url_link(amiibo:str):
+         return AmiiboAPI(amiibo)[0].get("image")
+
+class WebImage:
+     def __init__(self,url):
+          u = requests.get(url)
+          self.image = ImageTk.PhotoImage(Image.open(BytesIO(u.content)))
+          
+     def get(self):
+          return self.image
+
 my_amiibo = AmiiboAPI("Kirby")
 
 window = tk.Tk()
-window.title("Message Reverser")
-window.geometry("400x250")
-window.resizable(False, False)
+window.title("unc")
 
+img = WebImage(get_url_link("link")).get()
+
+label = tk.Label(window, image=img)
+label.pack(padx=20, pady=20)
 window.mainloop()
 print(my_amiibo)
--- tinkle.py
+++ tinkle.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
 window = tk.Tk()
 window.title("unc")
 
-photo = tk.PhotoImage(file="arab-money.png")
+image = Image.open("arab-money.png") # open image file
+photo = ImageTk.PhotoImage(image) # convert for Tkinter
 
 label = tk.Label(window, image=photo)
 label.pack(padx=20, pady=20)
